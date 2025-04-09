69bdd7fdd9772864e2b913cec853dc59172e5531
Lei Zhao <8961794+leizha@users.noreply.github.com>
2025-04-09 00:35:03 -0700

    tool call

--- src/llmutil/__init__.py
+++ src/llmutil/__init__.py
@@ -1,5 +1,7 @@
 from .llm import chat, gen
 from .schema import gen_arr, gen_bool, gen_num, gen_obj, gen_schema, gen_str
+from .tools import use_tools
+from .tools_def import tool_def
 
 __all__ = [
     "chat",
@@ -10,4 +12,6 @@ __all__ = [
     "gen_obj",
     "gen_schema",
     "gen_str",
+    "tool_def",
+    "use_tools",
 ]
